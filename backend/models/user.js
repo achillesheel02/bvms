11a8a3462de27687a701f76eac3e9f905b3abc9a
Barak Achillah <bachillah@gmail.com>
2020-05-14 15:31:01 +0300

    adding user table

---
+++ backend/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: false
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: false
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+    phoneNumber: {
+      type: Number,
+      required: false
+    },
+    role: [{
+      type: String,
+      required: true
+    }]}
+  ,{timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at'
+    }
+  });
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User',userSchema);
